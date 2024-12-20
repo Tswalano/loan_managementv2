25e40dd0ad7f46e5380325fc45352f139615b29c
Glen Bond Mogane <moganegb@gmail.com>
2024-12-21 00:11:54 +0200

    Remove VITE prefix

--- src/lib/utils/quries.ts
+++ src/lib/utils/quries.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import { transactions } from "../db/schema";
 
-const db = drizzle(import.meta.env.VITE_DATABASE_URL!);
+const db = drizzle(import.meta.env.DATABASE_URL!);
 
 // fect all loans from supabase
 export const loanData = await db.select().from(transactions);
